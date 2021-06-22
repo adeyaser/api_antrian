79ba8c4e1cddb19edafe79eef0cae0638ec7a890
adeyaser <59902463+adeyaser@users.noreply.github.com>
2021-06-22 12:57:17 +0700

    updatedb
    updatedb

--- dbantrian.sql
+++ dbantrian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2021 at 05:19 PM
+-- Generation Time: Jun 22, 2021 at 07:56 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -33,16 +33,17 @@ CREATE TABLE `acl_user` (
   `id_pegawai_pasien` varchar(16) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `role` varchar(10) NOT NULL
+  `role` varchar(10) NOT NULL,
+  `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `acl_user`
 --
 
-INSERT INTO `acl_user` (`id_acl`, `id_pegawai_pasien`, `username`, `password`, `role`) VALUES
-(1, '1234', 'Maria', '202cb962ac59075b964b07152d234b', 'admin'),
-(2, '3456', 'ade', '202cb962ac59075b964b07152d234b70', 'pasien');
+INSERT INTO `acl_user` (`id_acl`, `id_pegawai_pasien`, `username`, `password`, `role`, `group_id`) VALUES
+(1, '1234', 'Maria', '202cb962ac59075b964b07152d234b', 'admin', 1),
+(2, 'ade joko', 'adejoko', 'd41d8cd98f00b204e9800998ecf8427e', 'pasien', 3);
 
 -- --------------------------------------------------------
 
@@ -69,6 +70,35 @@ INSERT INTO `antrian` (`no_antrian`, `id_antrian`, `tanggal`, `id_pasien`, `id_p
 (3, 'A0003', '2021-06-21 10:10:08', '12345', '123', 'Y'),
 (4, 'A0004', '2021-06-21 18:12:07', '111111', '123', 'Y'),
 (5, 'A0005', '2021-06-21 21:11:08', '111123', '123', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listmenu`
+--
+
+CREATE TABLE `listmenu` (
+  `id_listmenu` int(11) NOT NULL,
+  `idetitas` varchar(20) NOT NULL,
+  `nama_listmenu` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL,
+  `group_id` varchar(2) NOT NULL,
+  `parent` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `listmenu`
+--
+
+INSERT INTO `listmenu` (`id_listmenu`, `idetitas`, `nama_listmenu`, `link`, `group_id`, `parent`) VALUES
+(1, 'Data Master', 'Master User', 'home/master_user', '1', '0'),
+(2, 'Data Master', 'Master Pegawai', 'home/master_pegawai', '1', '0'),
+(3, 'Data Master', 'Master Pasien', 'home/master_pasien', '1', '0'),
+(4, 'Data Antrian', 'Antrian', 'home/antrian', '1', '0'),
+(5, 'Laporan', 'Laporan Antrian', 'home/laporan_antrian', '1', '0'),
+(6, 'Data Antrian', 'Antrian', 'home/antrian', '2', '0'),
+(7, 'Pengaturan', 'Ganti Password', 'home/gantipassword', '1', '0'),
+(8, 'Pengaturan', 'Ganti Password', 'home/gantipassword', '2', '0');
 
 -- --------------------------------------------------------
 
@@ -131,6 +161,12 @@ ALTER TABLE `antrian`
   ADD PRIMARY KEY (`no_antrian`);
 
 --
+-- Indexes for table `listmenu`
+--
+ALTER TABLE `listmenu`
+  ADD PRIMARY KEY (`id_listmenu`);
+
+--
 -- Indexes for table `pasien`
 --
 ALTER TABLE `pasien`
@@ -157,6 +193,12 @@ ALTER TABLE `acl_user`
 --
 ALTER TABLE `antrian`
   MODIFY `no_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `listmenu`
+--
+ALTER TABLE `listmenu`
+  MODIFY `id_listmenu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
